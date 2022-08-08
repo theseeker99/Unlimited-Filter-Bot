d5da9ce589fe80f5331fcc4425805fa4be1fffd8
theseeker99 <110098713+theseeker99@users.noreply.github.com>
2022-08-08 21:32:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://github.com/theseeker99/Unlimited-Filter-Bot")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
